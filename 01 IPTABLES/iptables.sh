a0eeb01d7759c58870b29b82281d8c2723fe5f98
mike <michael.genesini@gmail.com>
2015-03-18 12:02:54 +0100

    test

--- 01 IPTABLES/iptables.sh
+++ 01 IPTABLES/iptables.sh
@@ -7,21 +7,18 @@ echo 'Ciao Bel'
 
 default="10.10.10."
 mike=$default"20"
-marco=$default"30"
-marci=$default"Y"
-pedro=$default"Z"
+marco=$default"40"
+marci=$default"50"
+pedro=$default"30"
 router=$default"10"
-
 mikeMac="54:26:96:db:a4:ad"
-
-trusted=""
+openPorts="80,443,9091"
+mailPorts="143,993,110,995,25"
 
 # External Public Interface
-EXTIF="eth0"
-
+EXTIF="usb0"
 # Internal Private Interface
 INTIF="wlan0"
-
 # Internal IPs
 INTIPS=$default"0/24"
 
@@ -34,39 +31,67 @@ iptables -t mangle -F
 iptables -t mangle -X
 iptables -t nat -P PREROUTING ACCEPT
 iptables -t nat -P POSTROUTING ACCEPT
-iptables -P INPUT ACCEPT
-iptables -P OUTPUT ACCEPT
+iptables -P INPUT DROP
+iptables -P OUTPUT DROP
 iptables -P FORWARD ACCEPT
 
-# SISTEMARE IP
+iptables -A FORWARD -i $INTIF -o $EXTIF -j ACCEPT
+
 #
 # Nella rete interna tutte le chiamate sulla porta 80 al router vengono
 # ridirezionate sulla porta 8000 a Marco che ha un servizio che deve essere
 # accessibile a tutta la rete interna
 #
 iptables -t nat -A PREROUTING -i $INTIF -p tcp --dport 80 -d $router -s $INTIPS -j DNAT --to-destination $marco:8000
-
-
-# 10.10.10.139 pinga 10.10.10.X
-# 10.10.10.X vede che sta pingando con 10.10.10.214
-#
-# se 10.10.10.139 pinga 10.10.10.214
-# 10.10.10.214 vede che pinga con se stesso
-#iptables -t nat -A POSTROUTING -s 10.10.10.11 -j SNAT --to-source 10.10.10.9
+iptables -t nat -A PREROUTING -i $INTIF -p tcp -m multiport --dports $openPorts",22,"$mailPorts -j ACCEPT
 
 #
-# Droppo tutto tranne quello che arriva da 10.10.10.140
+# Blocca degli specifici indirizzi IP oppure host
 #
-#iptables -P INPUT DROP
-#iptables -A INPUT -s 10.10.10.9 -j ACCEPT
+#iptables -A OUTPUT -d www.facebook.com -j DROP
+#iptables -A OUTPUT -d facebook.com -j REJECT
+
 #
-
-#iptables -A OUTPUT -p tcp -d "www.facebook.com" -j DROP
-
+# Consente ssh da dentro la rete per tutti
+# Consente ssh da fuori solo ad uno specifico indirizzo MAC
+#
 iptables -A INPUT -i $INTIF -p tcp --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A INPUT -i $EXTIF -p tcp --dport 22 -m mac --mac-source $mikeMac -j ACCEPT
-iptables -A OUTPUT -o $INTIF -p tcp --dport 22 -m state --state ESTABLISHED -j ACCEPT
+iptables -A INPUT -i $EXTIF -p tcp --dport 22 -m mac --mac-source $mikeMac -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -o $INTIF -p tcp --sport 22 -m state --state ESTABLISHED -j ACCEPT
 
-iptables -A FORWARD -i $INTIF -o $EXTIF -j ACCEPT
+iptables -A INPUT -i $INTIF -p icmp -j ACCEPT
+#
+# Consente traffico MAIL
+# Consente IMAP/IMAP2, IMAPS, POP3, POP3S
+#
+iptables -A INPUT -i $INTIF -p tcp -m multiport --dports $mailPorts","$openPorts -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -o $INTIF -p tcp -m multiport --sports $mailPorts","$openPorts -m state --state ESTABLISHED -j ACCEPT
+
+#
+#
+#
+iptables -A OUTPUT -o $INTIF -p tcp -m multiport --dports $mailPorts","$openPorts -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A INPUT -i $INTIF -p tcp -m multiport --sports $mailPorts","$openPorts -m state --state ESTABLISHED -j ACCEPT
+
+
+#
+# Forward interfaccia da interna a esterna
+#
+#iptables -A FORWARD -i $INTIF -o $EXTIF -j ACCEPT
+
+iptables -A INPUT -p udp -i $INTIF --sport 53 -j ACCEPT
+iptables -A OUTPUT -p udp -o $INTIF --dport 53 -j ACCEPT
+
+iptables -A OUTPUT -o $INTIF -p icmp -j ACCEPT
+#iptables -A FORWARD -d facebook.com -j REJECT
+
 
 iptables -t nat -A POSTROUTING -s $INTIPS -j MASQUERADE
+
+#
+#
+# LOG
+iptables -N LOGGING
+iptables -A INPUT -j LOGGING
+iptables -A LOGGING -m limit --limit 2/min -j LOG --log-prefix "iptables Packet Dropped: " --log-level 7
+iptables -A LOGGING -j DROP
