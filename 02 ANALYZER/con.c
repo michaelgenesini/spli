946ba0e5f68ad6b3afbff2c07baca9c2565e5a67
Marco Stagni <marcoponds@gmail.com>
2015-03-19 08:54:46 +0100

    fixed con.c

--- 02 ANALYZER/con.c
+++ 02 ANALYZER/con.c
@@ -2,30 +2,32 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc,char **argv) {
-  FILE *fp;
-  unsigned long int lo;
-  unsigned short int sh;
-  unsigned char ch;
+int main(int argc, char **argv) {
+	FILE *fp;
+	unsigned long int lo;
+	unsigned short int sh;
+	unsigned char ch;
 
-  if(argc!=2) {
-    printf("Use: %s <file>\n",argv[0]);
-    exit(1);
-  }
+	if(argc != 2) {
+		printf("Use: %s <file>\n", argv[0]);
+		exit(1);
+	}
 
-  fp=fopen(argv[1],"rb");
-  if(fp==NULL) exit(1);
+	fp=fopen(argv[1], "rb");
+	if (fp == NULL)
+		exit(1);
 
-  fread(&lo,sizeof(unsigned long int),1,fp);
-  printf("%ld\n",lo);
+	fread(&lo, sizeof(unsigned long int), 1, fp);
+	printf("%ld\n", lo);
 
-  for(;;) {
-    fread(&lo,sizeof(unsigned long int),1,fp);
-    fread(&sh,sizeof(unsigned short int),1,fp);
-    fread(&ch,sizeof(unsigned char),1,fp);
-    if(feof(fp)) break;
-    printf("%-9ld %-4d %-2d\n",lo,sh,ch);
-  }
+	for(;;) {
+		fread(&lo, sizeof(unsigned long int), 1, fp);
+		fread(&sh, sizeof(unsigned short int), 1, fp);
+		fread(&ch, sizeof(unsigned char), 1, fp);
+		if(feof(fp))
+			break;
+		printf("%-9ld %-4d %-2d\n", lo, sh, ch);
+	}
 
-  fclose(fp);
+	fclose(fp);
 }
