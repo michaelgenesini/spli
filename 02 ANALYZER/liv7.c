8e387cbb7d4965e64b6c6791b1f1e71e6e6bc645
Marco Stagni <marcoponds@gmail.com>
2015-03-23 14:51:21 +0100

    increased size of packet to decode

--- 02 ANALYZER/liv7.c
+++ 02 ANALYZER/liv7.c
@@ -2,23 +2,30 @@
 #include<string.h>
 
 void liv7(u_int len,const u_char *p) {
-  int i;
+  int i = 1;
   if((int)len<=0) return;
   //increasing counter
   counter.lvl7++;
-
+  int not_readable = 0;
   colore(5);
   myprintf("APPL |");
-  for(i=1;i<=len;i++) {
-    if(isprint(*p))
-      myprintf("%c",*p);
-    else
+  for(i = 1; i <= len*2; i++) {
+    if(isprint(*p)) {
+      myprintf("%c", *p);
+      not_readable = 0;
+    } else {
       myprintf(".");
-    if(isascii(*p))
-      fprintf(mem,"%c",*p);
-    else
+      not_readable++;
+    }
+    if(isascii(*p)) {
+      fprintf(mem, "%c", *p);
+      not_readable = 0;
+    } else {
       fprintf(mem,".");
+      not_readable++;
+    }
     p++;
+    if (not_readable > 100) break;
     if((i%70)==0)
       myprintf("\n     |");
   }
