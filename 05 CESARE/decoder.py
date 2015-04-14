b8a4d33949b2db56932c8dc5006c2396a946751f
Michael <michael.genesini@gmail.com>
2015-04-14 23:18:22 +0200

    added bruteforce control

--- 05 CESARE/decoder.py
+++ 05 CESARE/decoder.py
@@ -1,5 +1,6 @@
 from utils import *
 
+import imghdr
 import traceback
 
 class Decoder:
@@ -28,7 +29,35 @@ class Decoder:
                 se estensione non in self.extensions allora continua con la prossima
                 iterazione, altrimenti abbiamo trovato (probabilmente) la chiave giusta
             '''
-            pass
+            output = self.file.split(".")[0] + "_decoded." + self.file.split(".")[1]
+            # open the input file
+            f = open(self.file, "rb")
+            f.seek(0)
+            for _key in range(1,255):
+                # creating output file
+                out = open(output, "wb")
+                out.seek(0)
+                try:
+                    byte = f.read(1)
+                    while byte != "":
+                        # ho letto un byte, lo stampo
+                        # processo il byte appena letto
+                        toWrite = dec(int(charToBin(byte), 2), _key, MIN)
+                        out.write(chr(toWrite))
+                        # ne leggo un altro
+                        byte = f.read(1)
+                except Exception, e:
+                    traceback.print_exc()
+                finally:
+                    # alla fine chiudo i files
+                    f.seek(0)
+                    out.close()
+                    pass
+                frm = str(imghdr.what(output))
+                if frm == "jpeg" or frm == "png":
+                    print "Key detected: " + str(_key)
+                    break
+            f.close()
         else:
             output = self.file.split(".")[0] + "_decoded." + self.file.split(".")[1]
             # open the input file
