da0511951aeef57606c9c29ad1f2e01ead101529
Marco Stagni <marcoponds@gmail.com>
2015-05-05 16:04:00 +0200

    updated massey omura, added IPNOROSPO

--- 07 MASSEY-OMURA/Pedro/massey_omura.sh
+++ 07 MASSEY-OMURA/Pedro/massey_omura.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 echo "1"
-python massey_omura.py Lena.tga Lena_EA.tga -e key.bin
+python massey_omura.py ipnorospo.tga ipnorospo_EA.tga -e key.bin
 
 echo "2"
-python massey_omura.py Lena_EA.tga Lena_EA_EB.tga -e key2.bin
+python massey_omura.py ipnorospo_EA.tga ipnorospo_EA_EB.tga -e key2.bin
 
 echo "3"
-python massey_omura.py Lena_EA_EB.tga Lena_EB.tga -d key.bin
+python massey_omura.py ipnorospo_EA_EB.tga ipnorospo_EB.tga -d key.bin
 
 echo "4"
-python massey_omura.py Lena_EB.tga Lena_decrypt.tga -d key2.bin
+python massey_omura.py ipnorospo_EB.tga ipnorospo_decrypt.tga -d key2.bin
