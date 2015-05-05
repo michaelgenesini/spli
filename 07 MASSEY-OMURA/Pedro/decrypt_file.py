cafc754274a22729d2f15026da876736cc932255
Marco Stagni <marcoponds@gmail.com>
2015-05-05 15:18:35 +0200

    completed massey omura FUCK YEAH

--- 07 MASSEY-OMURA/Pedro/decrypt_file.py
+++ 07 MASSEY-OMURA/Pedro/decrypt_file.py
@@ -9,59 +9,36 @@ import math
 
 def decrypt(key,inputfile,outputfile):
 
-	#im = Image.open(inputfile,"r")
-
-
-	#Prende i soli dati di un'immagine
-	#prova = list(im.getdata())
-
-	#print prova[0]
-
-	#converte lista di triplette in lista di elementi singoli
-	#res = [i[0] for i in prova] 
-
-	#converte lista in array di interi
-	#inpixel = []
-	#inpixel = [int(pix) for pix in res]
-
-
-	#Ora dovrei aggiungere la chiave ad ogni dato 
-	#i=0
-	#for element in inpixel:
-	#	element = element << key
-	#	print element,"\n"
-
-	#print inpixel[0]
-	#Dopo aver ricreato una lista di triplette basta fare il contrario:
-
-	#im2.putdata() e riesci a creare l'immagine con i pixel shiftati		
-
-
 	#Prova classica con apertura file binario
 	keyfile = open(key,"rb")
 	infile = open(inputfile,"rb")
 	outfile = open(outputfile,"wb")
+
+	# reading tga content
+	content = infile.read()
+	header = content[:18]
+	body = content[18:]
 	
-	infile_lenght = os.fstat(infile.fileno()).st_size
+	infile_lenght = len(body)#os.fstat(infile.fileno()).st_size
 
 	padbyte = bytearray(keyfile.read())
 	
-	byte = bytearray(infile.read())
+	#byte = bytearray(infile.read())
 
-	shiftn = 8-(padbyte[0]%8)
-	shiftn2 = 8-shiftn
-	shiftmask = int(math.pow(2, shiftn2))-1;
-	shiftmask2 = 255 ^ shiftmask
+	#shiftn = 8-(padbyte[0]%8)
+	#shiftn2 = 8-shiftn
+	#shiftmask = int(math.pow(2, shiftn2))-1;
+	#shiftmask2 = 255 ^ shiftmask
+	
+	# writing header
+	outfile.write(header)
 	
 	for i in range(0, infile_lenght):
-		byte[i] = byte[i] ^ padbyte[i]		
+		#byte[i] = byte[i] ^ padbyte[i]		
 		#temp = ((byte[i] & shiftmask) << shiftn) | ((byte[i] & shiftmask2) >> shiftn2)
 		#byte[i] = temp
-		outfile.write(chr(byte[i]))
+		outfile.write(chr(ord(body[i]) ^ padbyte[i]))
 	
 	keyfile.close()
 	infile.close()
 	outfile.close()
-
-
-	#Lo stesso problema c'e se hai 255 e sommi va in crisi
--- 07 MASSEY-OMURA/Pedro/encrypt_file.py
+++ 07 MASSEY-OMURA/Pedro/encrypt_file.py
@@ -2,66 +2,45 @@ import os
 import sys
 import string
 import math
-#from PIL import Image
-
-#NON FUNZIONA ANCORA
-#brew install pillow
 
 def encrypt(key,inputfile,outputfile):
-
-	#im = Image.open(inputfile,"r")
-
-
-	#Prende i soli dati di un'immagine
-	#prova = list(im.getdata())
-
-	#print prova[0]
-
-	#converte lista di triplette in lista di elementi singoli
-	#res = [i[0] for i in prova] 
-
-	#converte lista in array di interi
-	#inpixel = []
-	#inpixel = [int(pix) for pix in res]
-
-
-	#Ora dovrei aggiungere la chiave ad ogni dato 
-	#i=0
-	#for element in inpixel:
-	#	element = element << key
-	#	print element,"\n"
-
-	#print inpixel[0]
-	#Dopo aver ricreato una lista di triplette basta fare il contrario:
-
-	#im2.putdata() e riesci a creare l'immagine con i pixel shiftati		
-
 
 	#Prova classica con apertura file binario
 	keyfile = open(key,"wb")
 	infile = open(inputfile,"rb")
 	outfile = open(outputfile,"wb")
 
+	# reading tga content
+	content = infile.read()
+	header = content[:18]
+	body = content[18:]
+
+	# generating random key
 	randomGen = open('/dev/urandom','rb')
-	infile_lenght = os.fstat(infile.fileno()).st_size
+	infile_lenght = len(body)#os.fstat(infile.fileno()).st_size
 	pad = randomGen.read(infile_lenght)
 	keyfile.write(pad)
 	keyfile.close()
+	randomGen.close()
 	
+	# creating padbyte for encription
 	padbyte = bytearray(pad)
 	
-	byte = bytearray(infile.read())
+	#byte = bytearray(infile.read())
 
-	shiftn = padbyte[0]%8
-	shiftn2 = 8-shiftn
-	shiftmask = int(math.pow(2, shiftn2))-1;
-	shiftmask2 = 255 ^ shiftmask
+	#shiftn = padbyte[0]%8
+	#shiftn2 = 8-shiftn
+	#shiftmask = int(math.pow(2, shiftn2))-1;
+	#shiftmask2 = 255 ^ shiftmask
+
+	# writing header
+	outfile.write(header)
 	
 	for i in range(0, infile_lenght):
 		#temp = ((byte[i] & shiftmask) << shiftn) | ((byte[i] & shiftmask2) >> shiftn2)
 		#byte[i] = temp
-		outfile.write(chr(byte[i] ^ padbyte[i]))
-	
+		outfile.write(chr(ord(body[i]) ^ padbyte[i]))
+
 	infile.close()
 	outfile.close()
 
