93fe9682c8ff91600916893b7ec95863980ab50c
Enrico Pedroni <enriped@gmail.com>
2015-05-06 15:22:09 +0200

    massey omura finito

--- 07 MASSEY-OMURA/Pedro/massey_omura.sh
+++ 07 MASSEY-OMURA/Pedro/massey_omura.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-echo "1"
+echo "Cripto con chiave Ka"
 python massey_omura.py Lena.tga Lena_A.tga -e key.bin
 
-echo "2"
+echo "Cripto con chiave Kb"
 python massey_omura.py Lena_A.tga Lena_AB.tga -e key2.bin
 
-echo "3"
+echo "Decripto con chiave Ka"
 python massey_omura.py Lena_AB.tga Lena_B.tga -d key.bin
 
-echo "4"
+echo "Decripto con chiave Kb"
 python massey_omura.py Lena_B.tga Lena_decrypt.tga -d key2.bin
