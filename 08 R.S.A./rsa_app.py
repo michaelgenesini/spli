806b659df950fd104404093fb7d7c2b55e032c34
Michael <michael.genesini@gmail.com>
2015-05-12 12:56:42 +0200

    updated rsa_app

--- 08 R.S.A./rsa_app.py
+++ 08 R.S.A./rsa_app.py
@@ -13,28 +13,24 @@ def index():
 @app.expose("/encode/<str:filename>/<int:lenk>")
 def rsa(filename,lenk):
 	title = 'R.S.A.'
-	print 'Encoding ...'
-	print 'Filename: ',filename
-	print 'Len Key: ',lenk
-	cmd = ["python3","rsa.py", "encode", filename, lenk]
-	print 'TEST: ', cmd
-	p = subprocess.Popen(cmd,stdout = subprocess.PIPE,stderr=subprocess.PIPE,stdin=subprocess.PIPE)
-	out,err = p.communicate()
+	out, err = encode(filename,lenk)
 	title = 'R.S.A.'
-	data = {'title': title, 'out': out, 'err': err}
+	data = {'title': title, 'filename': filename, 'out': [s.strip() for s in out.splitlines()], 'err': err}
 	return data
 
-@app.expose("/decode")
-def rsa():
-	title = 'R.S.A. Decode'
-	data = {'title': title}
-	return data
-
-@app.expose("/bruteforce")
-def bruteforce():
+@app.expose("/bruteforce/<str:filename>/<int:lenk>")
+def bruteforce(filename,lenk):
 	title = 'R.S.A. Bruteforce'
 	data = {'title': title}
 	return data
+
+def encode(filename, lenk):
+	pathtofile = "static/imgs/"+filename
+	print pathtofile
+	cmd = ["python3","rsa.py", "encode", pathtofile, lenk]
+	p = subprocess.Popen(cmd,stdout = subprocess.PIPE,stderr=subprocess.PIPE,stdin=subprocess.PIPE)
+	out,err = p.communicate()
+	return (out, err)
 
 if __name__ == "__main__":
 	app.run()
